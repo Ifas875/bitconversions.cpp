3e2a53e3a99c9c1b41e9cc9b85a0f531b4ea275e
Safi <safi.sharieff01@gmail.com>
2026-03-06 21:36:31 +0400

    Added rough octal implementation

--- bitwise.cpp
+++ bitwise.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <algorithm>
 
 
 int main(){
-  int a = 5;
+  int a = 510;
   int b = a & 7;
-  std::cout << a << " " << b <<std::endl;
+  std::cout << a << " " << b << std::endl;
 
+  std::string answer;
+
+  while (a>0){
+    std::cout << a << std::endl;
+    int temp = a & 7;
+    answer += std::to_string(temp);
+    a = a >> 3;
+  }
+  std::reverse(answer.begin(), answer.end());
+
+  std::cout << answer << std::endl;
   return 0;
 }
